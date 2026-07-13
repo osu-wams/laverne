631428d1f69f6d308d8d8cdcf2585b0a9eadb0e9
Matthew Brabham <matthew.brabham@oregonstate.edu>
2026-07-13 14:17:00 -0700

    Updating code standards

--- src/Drush/Commands/LaverneChangeOwnerCommand.php
+++ src/Drush/Commands/LaverneChangeOwnerCommand.php
@@ -2,10 +2,11 @@
 
 namespace Drupal\laverne\Drush\Commands;
 
-use Consolidation\OutputFormatters\FormatterManager;
 use Drupal\Core\Database\Connection;
 use Drush\Commands\AutowireTrait;
 use Drush\Formatters\FormatterTrait;
+use Drush\Style\DrushStyle;
+use Override;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
@@ -14,94 +15,75 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(
-  name: self::NAME,
-  description: 'Update the owner of a shURLy URL slug.',
-  aliases: ['lco'],
+    name: self::NAME,
+    description: 'Update the owner of a shURLy URL slug.',
+    aliases: ['lco'],
 )]
-class LaverneChangeOwnerCommand extends Command {
+final class LaverneChangeOwnerCommand extends Command
+{
 
-  use AutowireTrait;
-  use FormatterTrait;
+    use AutowireTrait;
+    use FormatterTrait;
 
-  /**
-   * Drush command name.
-   */
-  public const string NAME = 'laverne:change-owner';
+    public const string NAME = 'laverne:change-owner';
 
-  /**
-   * Constructor method to initialize the object with the required database
-   * connection.
-   *
-   * @param Connection $db The database connection instance.
-   *
-   * @return void
-   */
-  public function __construct(
-    private readonly FormatterManager $formatterManager,
-    private readonly Connection $db,
-    private readonly LoggerInterface $logger
-  ) {
-    parent::__construct();
-  }
-
-  /**
-   * @param \Symfony\Component\Console\Input\InputInterface $input
-   * @param \Symfony\Component\Console\Output\OutputInterface $output
-   * @param string $onid
-   * @param string $url
-   *
-   * @return bool
-   * @throws \Exception
-   */
-  public function doExecute(InputInterface $input, OutputInterface $output, string $onid, string $url): bool {
-    $uid = $this->db->select('authmap', 'am')
-      ->fields('am', ['uid'])
-      ->condition('am.authname', $onid)
-      ->execute()
-      ->fetchField();
-    if (!$uid) {
-      $this->logger->error("No user found with ONID '{onid}'.", [
-        '%onid' => $onid,
-      ]);
-      return FALSE;
+    public function __construct(
+        private readonly Connection $db,
+        private readonly LoggerInterface $logger
+    ) {
+        parent::__construct();
     }
-    else {
-      $this->db->update('shurly')
-        ->fields(['uid' => $uid])
-        ->condition('source', $url)
-        ->execute();
-      $this->logger->info("Owner of shURLy URL slug '{url}' changed to ONID '{onid}'.", [
-        '%url' => $input->getArgument('url'),
-        '%onid' => $input->getArgument('onid'),
-      ]);
-      return TRUE;
-    }
-  }
 
-  /**
-   * {@inheritDoc}
-   */
-  public function execute(InputInterface $input, OutputInterface $output): int {
-    $results = $this->doExecute($input, $output, $input->getArgument('onid'), $input->getArgument('url'));
-    if ($results) {
-      $output->writeln("Changed owner of {$input->getArgument('url')} to ONID {$input->getArgument('onid')}");
-      return Command::SUCCESS;
+    public function doExecute(InputInterface $input, OutputInterface $output, string $onid, string $url): bool
+    {
+        $uid = $this->db->select('authmap', 'am')
+            ->fields('am', ['uid'])
+            ->condition('am.authname', $onid)
+            ->execute()
+            ->fetchField();
+        if (!$uid) {
+            $this->logger->error(
+                "No user found with ONID '{onid}'.", [
+                '%onid' => $onid,
+                ]
+            );
+            return false;
+        }
+        else {
+            $this->db->update('shurly')
+                ->fields(['uid' => $uid])
+                ->condition('source', $url)
+                ->execute();
+            $this->logger->info(
+                "Owner of shURLy URL slug '{url}' changed to ONID '{onid}'.", [
+                '%url' => $input->getArgument('url'),
+                '%onid' => $input->getArgument('onid'),
+                ]
+            );
+            return true;
+        }
     }
-    $output->writeln("Failed to change owner of {$input->getArgument('url')} to ONID {$input->getArgument('onid')}");
-    return Command::FAILURE;
-  }
 
-  /**
-   * Configures the command with help text, required arguments, and usage
-   * information.
-   *
-   * @return void
-   */
-  protected function configure(): void {
-    $this->setHelp("Change the owner of a shURLy URL slug.")
-      ->addArgument("onid", InputArgument::REQUIRED, "A user's ONID")
-      ->addArgument("url", InputArgument::REQUIRED, "The shURLy URL slug to chang the owner for.")
-      ->addUsage("laverne:change-owner beaverb aBc");
-  }
+    #[Override]
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new DrushStyle($input, $output);
+        $results = $this->doExecute($input, $output, $input->getArgument('onid'), $input->getArgument('url'));
+        if ($results) {
+            $io->success(sprintf("Changed owner of %s to ONID %s", $input->getArgument('url'), $input->getArgument("onid")));
+            return Command::SUCCESS;
+        }
+        $io->error(sprintf("Failed to change owner of %s to ONID %s", $input->getArgument("url"), $input->getArgument("onid")));
+        return Command::FAILURE;
+    }
+
+    #[Override]
+    protected function configure(): void
+    {
+        $this->setHelp("Change the owner of a shURLy URL slug.")
+            ->addArgument("onid", InputArgument::REQUIRED, "A user's ONID")
+            ->addArgument("url", InputArgument::REQUIRED, "The shURLy URL slug to chang the owner for.")
+            ->addUsage("laverne:change-owner beaverb aBc");
+    }
 
 }
--- src/Drush/Commands/LaverneCheckOwnerCommand.php
+++ src/Drush/Commands/LaverneCheckOwnerCommand.php
@@ -2,12 +2,12 @@
 
 namespace Drupal\laverne\Drush\Commands;
 
-use Consolidation\OutputFormatters\FormatterManager;
 use Consolidation\OutputFormatters\StructuredData\RowsOfFields;
 use Drupal\Core\Database\Connection;
 use Drush\Attributes as CLI;
 use Drush\Commands\AutowireTrait;
 use Drush\Formatters\FormatterTrait;
+use Override;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -15,79 +15,74 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(
-  name: self::NAME,
-  description: 'Check the current owner of a shURLy URL slug.',
-  aliases: ['lcko'],
+    name: self::NAME,
+    description: 'Check the current owner of a shURLy URL slug.',
+    aliases: ['lcko'],
 )]
-#[CLI\FieldLabels(labels: [
-  'url' => 'Short URL',
-  'onid' => 'ONID',
-  'owner' => 'Owner',
-  'destination' => 'Destination',
-])]
+#[CLI\FieldLabels(
+    labels: [
+        'url' => 'Short URL',
+        'onid' => 'ONID',
+        'owner' => 'Owner',
+        'destination' => 'Destination',
+    ]
+)]
 #[CLI\DefaultTableFields(fields: ['url', 'onid', 'owner', 'destination'])]
 #[CLI\FilterDefaultField(field: 'owner')]
 #[CLI\Formatter(returnType: RowsOfFields::class, defaultFormatter: 'table')]
-class LaverneCheckOwnerCommand extends Command {
+final class LaverneCheckOwnerCommand extends Command
+{
 
-  use AutowireTrait;
-  use FormatterTrait;
+    use AutowireTrait;
+    use FormatterTrait;
 
-  public const string NAME = 'laverne:check-owner';
+    public const string NAME = 'laverne:check-owner';
 
-  public function __construct(
-    private readonly FormatterManager $formatterManager,
-    private readonly Connection $db,
-  ) {
-    parent::__construct();
-  }
+    public function __construct(
+        private readonly Connection $db,
+    ) {
+        parent::__construct();
+    }
 
-  /**
-   * {@inheritDoc}
-   */
-  public function execute(InputInterface $input, OutputInterface $output): int {
-    $results = $this->doExecute($input, $output, (string) $input->getArgument('url'));
+    #[Override]
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $results = $this->doExecute($input, $output, (string) $input->getArgument('url'));
 
-    $this->writeFormattedOutput($input, $output, $results);
-    return Command::SUCCESS;
-  }
+        $this->writeFormattedOutput($input, $output, $results);
+        return Command::SUCCESS;
+    }
 
-  /**
-   * Executes a database query to retrieve specific information based on the given URL
-   * and returns the result formatted as rows of fields.
-   *
-   * @param InputInterface $input The input interface instance.
-   * @param OutputInterface $output The output interface instance.
-   * @param string $url The URL used as a condition to filter query results.
-   *
-   * @return RowsOfFields A collection of rows containing the retrieved fields.
-   */
-  public function doExecute(InputInterface $input, OutputInterface $output, string $url): RowsOfFields {
-    $query = $this->db->select('shurly', 's');
-    $query->fields('s', ['uid', 'source', 'destination']);
-    $query->fields('u', ['name']);
-    $query->fields('am', ['authname']);
-    $query->join('users_field_data', 'u', 'u.uid = s.uid');
-    $query->join('authmap', 'am', 'am.uid = u.uid');
-    $query->condition('s.source', $url);
-    $owner = $query->execute()->fetchObject();
-    return new RowsOfFields([
-      [
-        'url' => $owner->source,
-        'owner' => $owner->name,
-        'onid' => $owner->authname,
-        'destination' => $owner->destination,
-      ],
-    ]);
-  }
+    #[Override]
+    protected function configure()
+    {
+        $this->addArgument("url", InputArgument::REQUIRED, "The URL to check the current owner of.");
+    }
 
-  /**
-   * Configures the command by defining its arguments and options.
-   *
-   * @return void
-   */
-  protected function configure() {
-    $this->addArgument("url", InputArgument::REQUIRED, "The URL to check the current owner of.");
-  }
+    /**
+     * Executes a database query to retrieve specific information based on the given URL
+     * and returns the result formatted as rows of fields.
+     */
+    protected function doExecute(InputInterface $input, OutputInterface $output, string $url): RowsOfFields
+    {
+        $query = $this->db->select('shurly', 's');
+        $query->fields('s', ['uid', 'source', 'destination']);
+        $query->fields('u', ['name']);
+        $query->fields('am', ['authname']);
+        $query->join('users_field_data', 'u', 'u.uid = s.uid');
+        $query->join('authmap', 'am', 'am.uid = u.uid');
+        $query->condition('s.source', $url);
+        $owner = $query->execute()->fetchObject();
+        return new RowsOfFields(
+            [ $owner->source =>
+                [
+                    'url' => $owner->source,
+                    'owner' => $owner->name,
+                    'onid' => $owner->authname,
+                    'destination' => $owner->destination,
+                ],
+            ]
+        );
+    }
 
 }
